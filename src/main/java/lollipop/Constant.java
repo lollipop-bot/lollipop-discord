3afbd6463b6fc9c64f8e1f23c979af0422d02a8f
boole <samarthupadhya2309@gmail.com>
2023-01-09 00:09:11 -0500

    constant updates

--- src/main/java/lollipop/Constant.java
+++ src/main/java/lollipop/Constant.java
@@ -30,6 +30,6 @@ public class Constant {
     public static double MULTIPLIER = 1.5d;
 
     // Software Version Value
-    public static String VERSION = "v2.10.4";
+    public static String VERSION = "v2.10.6";
 
 }
