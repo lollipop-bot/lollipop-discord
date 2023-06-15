6fda899c2942b1d9d84ebb1f50456cbeb6e7e501
boole <samarthupadhya2309@gmail.com>
2023-06-14 23:52:59 -0400

    fixed api command lag from deques.

--- src/main/java/awatch/controller/AListener.java
+++ src/main/java/awatch/controller/AListener.java
@@ -3,31 +3,33 @@ package awatch.controller;
 import awatch.model.*;
 import awatch.model.Character;
 import awatch.model.Question;
+import net.dv8tion.jda.api.events.interaction.component.SelectMenuInteractionEvent;
+import net.dv8tion.jda.api.interactions.InteractionHook;
 
 import java.util.ArrayList;
 
 public interface AListener {
 
-    void sendSearchAnime(ArrayList<Anime> animes);
-    void sendSearchCharacter(ArrayList<Character> character);
-    void sendSearchUser(User user);
-    void sendCharacterAnimes(Character character);
-    void sendCharacterMangas(Character character);
-    void sendCharacterVoices(Character character);
-    void sendRandomQuote(Quote quote);
-    void sendEpisodes(ArrayList<Episode> episodes);
-    void sendCharacterList(ArrayList<Character> characters);
-    void sendNews(ArrayList<Article> articles);
-    void sendStatistics(Statistic statistics);
-    void sendThemes(Themes themes);
-    void sendRecommendation(Recommendation recommendation);
-    void sendReview(Review review);
-    void sendTopAnime(ArrayList<Anime> top);
-    void sendPopularAnime(ArrayList<Anime> popular);
-    void sendLatestAnime(ArrayList<Anime> latest);
-    void sendRandomAnime(Anime random);
-    void sendRandomCharacter(Character random);
-    void sendRandomGIF(GIF gif);
-    void sendTrivia(Question question);
+    void sendSearchAnime(ArrayList<Anime> animes, InteractionHook message);
+    void sendSearchCharacter(ArrayList<Character> character, InteractionHook message);
+    void sendSearchUser(User user, InteractionHook message);
+    void sendCharacterAnimes(Character character, SelectMenuInteractionEvent event);
+    void sendCharacterMangas(Character character, SelectMenuInteractionEvent event);
+    void sendCharacterVoices(Character character, SelectMenuInteractionEvent event);
+    void sendRandomQuote(Quote quote, InteractionHook message);
+    void sendEpisodes(ArrayList<Episode> episodes, SelectMenuInteractionEvent event);
+    void sendCharacterList(ArrayList<Character> characters, SelectMenuInteractionEvent event);
+    void sendNews(ArrayList<Article> articles, SelectMenuInteractionEvent event);
+    void sendStatistics(Statistic statistics, SelectMenuInteractionEvent event);
+    void sendThemes(Themes themes, SelectMenuInteractionEvent event);
+    void sendRecommendation(Recommendation recommendation, SelectMenuInteractionEvent event);
+    void sendReview(Review review, SelectMenuInteractionEvent event);
+    void sendTopAnime(ArrayList<Anime> top, InteractionHook message);
+    void sendPopularAnime(ArrayList<Anime> popular, InteractionHook message);
+    void sendLatestAnime(ArrayList<Anime> latest, InteractionHook message);
+    void sendRandomAnime(Anime random, InteractionHook message);
+    void sendRandomCharacter(Character random, InteractionHook message);
+    void sendRandomGIF(GIF gif, InteractionHook message);
+    void sendTrivia(Question question, InteractionHook message);
 
 }
--- src/main/java/mread/controller/RListener.java
+++ src/main/java/mread/controller/RListener.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 import mread.model.Chapter;
 import mread.model.Manga;
+import net.dv8tion.jda.api.events.interaction.component.SelectMenuInteractionEvent;
+import net.dv8tion.jda.api.interactions.InteractionHook;
 
 public interface RListener {
 
@@ -11,42 +13,42 @@ public interface RListener {
      * Sends a manga list to the listener
      * @param mangas list of mangas
      */
-	void sendMangas(ArrayList<Manga> mangas);
+	void sendMangas(ArrayList<Manga> mangas, InteractionHook message);
 
     /**
      * Sends a popular manga list to the listener
      * @param popular list of mangas
      */
-    void sendPopularManga(ArrayList<Manga> popular);
+    void sendPopularManga(ArrayList<Manga> popular, InteractionHook message);
 
     /**
      * Sends a top manga list to the listener
      * @param top list of mangas
      */
-    void sendTopManga(ArrayList<Manga> top);
+    void sendTopManga(ArrayList<Manga> top, InteractionHook message);
 
     /**
      * Sends a latest manga list to the listener
      * @param latest list of mangas
      */
-    void sendLatestManga(ArrayList<Manga> latest);
+    void sendLatestManga(ArrayList<Manga> latest, InteractionHook message);
 
     /**
      * Sends a list of chapters
      * @param chapters list of chapters
      */
-	void sendChapters(ArrayList<Chapter> chapters);
+	void sendChapters(ArrayList<Chapter> chapters, SelectMenuInteractionEvent event);
 
     /**
      * Sends a list of pages
      * @param pages list of pages
      */
-	void sendPages(ArrayList<String> pages);
+	void sendPages(ArrayList<String> pages, SelectMenuInteractionEvent event);
 
     /**
      * Sends a random manga from MAL
      * @param manga random manga
      */
-    void sendRandomManga(Manga manga);
+    void sendRandomManga(Manga manga, InteractionHook message);
 
 }
