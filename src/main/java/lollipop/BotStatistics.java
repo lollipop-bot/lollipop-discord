b0ea1bf5cc63ae8b8ba12a56a62c49910bcd37fe
boole <samarthupadhya2309@gmail.com>
2023-06-14 23:51:45 -0400

    statuspage heartbeat

--- src/main/java/lollipop/BotStatistics.java
+++ src/main/java/lollipop/BotStatistics.java
@@ -1,5 +1,6 @@
 package lollipop;
 
+import awatch.controller.AConstants;
 import lollipop.Constant;
 import lollipop.Secret;
 import net.dv8tion.jda.api.events.ReadyEvent;
@@ -14,7 +15,11 @@ import org.discordbots.api.client.DiscordBotListAPI;
 import org.jetbrains.annotations.NotNull;
 import threading.ThreadManagement;
 
+import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.stream.Collectors;
 
 public class BotStatistics extends ListenerAdapter {
@@ -73,7 +78,7 @@ public class BotStatistics extends ListenerAdapter {
         // Setup Infinity Bot List Statistics and Server Count
         client = HttpClientBuilder.create().build();
         try {
-            HttpPost request = new HttpPost("https://api.infinitybotlist.com/bots/stats");
+            HttpPost request = new HttpPost("https://spider.infinitybots.gg/bots/stats");
             request.addHeader("content-type", "application/json");
             request.addHeader("authorization", Secret.INFINITYBOTLIST);
             request.addHeader("Accept", "application/json");
@@ -87,6 +92,21 @@ public class BotStatistics extends ListenerAdapter {
         }
         finally {
             // @Deprecated client.getConnectionManager().shutdown();
+        }
+
+        // Statuspage heartbeat
+        client = HttpClientBuilder.create().build();
+        try {
+            URL web = new URL("https://uptime.betterstack.com/api/v1/heartbeat/F7UDxUpdNi91efmmdJriY4zN");
+            HttpsURLConnection con = (HttpsURLConnection) web.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("Content-Type", "application/json");
+            con.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36");
+            con.setConnectTimeout(5000); // Sets Connection Timeout to 5 seconds
+            con.setReadTimeout(5000); // Sets Read Timeout to 5 seconds
+            con.getInputStream();
+        } catch(IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
